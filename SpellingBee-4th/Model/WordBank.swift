1eea2b8eaecd8824d62d8afa37763b394fbeb5d0
Eric Hernandez <eands9@yahoo.com>
2019-01-13 20:42:03 -0600

    french5

--- SpellingBee-4th/Model/WordBank.swift
+++ SpellingBee-4th/Model/WordBank.swift
@@ -12,15 +12,15 @@ class WordBank{
     var list = [Word]()
     
     init(){
-        list.append(Word(word:"prairie", sentence:""))
-        list.append(Word(word:"diorama", sentence:""))
-        list.append(Word(word:"entourage", sentence:""))
-        list.append(Word(word:"fuselage", sentence:""))
-        list.append(Word(word:"boudoir", sentence:""))
-        list.append(Word(word:"collage", sentence:""))
-        list.append(Word(word:"amenable", sentence:""))
-        list.append(Word(word:"expertise", sentence:""))
-        list.append(Word(word:"matinee", sentence:""))
-        list.append(Word(word:"plateau", sentence:""))
+        list.append(Word(word:"sortie", sentence:""))
+        list.append(Word(word:"croquette", sentence:""))
+        list.append(Word(word:"physique", sentence:""))
+        list.append(Word(word:"elite", sentence:""))
+        list.append(Word(word:"deluxe", sentence:""))
+        list.append(Word(word:"nougat", sentence:""))
+        list.append(Word(word:"rouge", sentence:""))
+        list.append(Word(word:"escargot", sentence:""))
+        list.append(Word(word:"crochet", sentence:""))
+        list.append(Word(word:"regime", sentence:""))
     }
 }
